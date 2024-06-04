cb81b39b9c8598bb532f9e6ae0ec4184c7b5f63b
Hossam El_Deen Kamal <101903513+HossamElDeenKamal2003@users.noreply.github.com>
2024-06-05 02:31:03 +0300

    Committing files

---
+++ CodeForces/C++17 (GCC 7-32)/432A | Choosing Teams/190485999.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+void Hossam() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+/*void solve() {
+    int  n, a[100];
+        cin >> n;
+        vector<int>vec;
+        for (int i = 0; i < n; i++) {
+            int x;
+            cin >> x;
+            vec.push_back(x);
+        }
+        sort(vec.begin(), vec.end());
+        vector<int> vec2;
+        int diff = 0;
+        for (int i = 1; i < n; i++) {
+            diff = vec[i] - vec[i - 1];
+            vec2.push_back(diff);
+        }
+        sort(vec2.begin(), vec2.end());
+        cout << vec2[0] << endl;
+}*/
+/*int main() {
+    int t, n, a[100];
+    cin >> t;
+    while (t--) {
+        cin >> n;
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        sort(a, a + n);
+        vector<int> v;
+        int diff = 0;
+        for (int i = 1; i < n; i++) {
+            diff = a[i] - a[i - 1];
+            v.push_back(diff);
+        }
+        sort(v.begin(), v.end());
+        cout << v[0] << endl;
+    }
+}*/
+int main() {
+    Hossam();
+  /*int t, num1, num2, m, n, res;
+    cin >> t;
+    while (t--) {
+        cin >> num1 >> num2;
+        if (num1 > num2)
+            swap(num1, num2);
+        m = max((num1 * 2), num2);
+        n = max((num1 * 2), num2);
+        res = m * n;
+        cout << m * n<<'\n';
+    }
+    */
+    int n, k, per, ctr=0;
+    cin >> n >> k;
+    for (int i = 0; i < n; i++) {
+        cin >> per;
+        if (per+k <= 5)
+            ctr++;
+    }
+    cout << ctr / 3 << '\n';
+}
