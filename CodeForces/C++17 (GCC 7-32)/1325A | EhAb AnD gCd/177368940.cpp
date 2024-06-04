fc3a3e7195ccd0a70963c9228161300ae3789eb2
Hossam El_Deen Kamal <101903513+HossamElDeenKamal2003@users.noreply.github.com>
2024-06-05 02:36:39 +0300

    Committing files

---
+++ CodeForces/C++17 (GCC 7-32)/1325A | EhAb AnD gCd/177368940.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#define ll long long
+#define el endl
+using namespace std;
+ 
+int main()
+{
+    ll t;
+    cin>>t;
+    while(t--){
+    ll n,y;
+    cin>>n;
+    cout<<1<<" "<<n-1<<el;
+    }
+    return 0;
+}
+ 
