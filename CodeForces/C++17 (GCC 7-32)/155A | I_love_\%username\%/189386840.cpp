b1a9ee0061604a2abf33f21ba07a84e09293c448
Hossam El_Deen Kamal <101903513+HossamElDeenKamal2003@users.noreply.github.com>
2024-06-05 02:32:11 +0300

    Committing files

---
+++ CodeForces/C++17 (GCC 7-32)/155A | I_love_\%username\%/189386840.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    int n,ctr=0;
+    cin>>n;
+    int arr[n];
+    for(int i=0; i<n; i++)
+    {
+        cin>>arr[i];
+    }
+    int max = arr[0];
+    int min = arr[0];
+    for(int i=0; i<n; i++)
+    {
+        if(arr[i]>max)
+        {
+            max=arr[i];
+            ctr++;
+        }
+        if(arr[i]<min)
+        {
+            min=arr[i];
+            ctr++;
+        }
+    }
+    cout<<ctr;
+    return 0;
+}
