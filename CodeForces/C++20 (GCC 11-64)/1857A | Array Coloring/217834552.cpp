97b8969dc58ff4798b659e7ab80bcab95334c8c2
Hossam El_Deen Kamal <101903513+HossamElDeenKamal2003@users.noreply.github.com>
2024-06-05 02:21:54 +0300

    Committing files

---
+++ CodeForces/C++20 (GCC 11-64)/1857A | Array Coloring/217834552.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <limits>
+#include <map>
+using namespace std;
+class clsSolve {
+private:
+    int number() {
+        int n;
+        cin >> n;
+        return n;
+    }
+    int n = number(), ctr = 0;
+    vector<int>vec;
+public:
+    clsSolve() {
+        for (int i = 0; i < n; i++) {
+            int x;
+            cin >> x;
+            vec.push_back(x);
+        }
+    }
+    void Process() {
+        for (int i = 0; i < n; i++) {
+            ctr += vec[i];
+        }
+    }
+    void Print() {
+        if (ctr % 2 == 0)
+            cout << "Yes\n";
+        else
+            cout << "NO\n";
+    }
+};
+int main() {
+    static int t;
+    cin >> t;
+    while (t--) {
+        clsSolve solve;
+        solve.Process();
+        solve.Print();
+    }
+    return 0;
+}
