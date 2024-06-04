1b0a4b9de1bb670875270c07a105773f8c8b16a2
Hossam El_Deen Kamal <101903513+HossamElDeenKamal2003@users.noreply.github.com>
2024-06-05 02:30:37 +0300

    Committing files

---
+++ CodeForces/C++17 (GCC 7-32)/1760D | Challenging Valleys/190759509.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+ 
+int main()
+{
+ int t;
+ cin >> t;
+ while (t--)
+ {
+  int n, a1, a, s = 0;
+  cin >> n >> a1;
+  for (int i = 1; i < n; i++)
+  {
+   cin >> a;
+   if (a1 < a)
+    s = max(s, 1);
+   if (a1 > a && s)
+    s = 2;
+   a1 = a;
+  }
+  if (s <= 1) 
+   cout << "YES" << endl;
+  else
+   cout << "NO" << endl;
+ }
+}
