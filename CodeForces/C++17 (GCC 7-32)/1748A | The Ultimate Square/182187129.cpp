a08e8dc21835e7e6c387cab55a9977ffbb7320e3
Hossam El_Deen Kamal <101903513+HossamElDeenKamal2003@users.noreply.github.com>
2024-06-05 02:34:45 +0300

    Committing files

---
+++ CodeForces/C++17 (GCC 7-32)/1748A | The Ultimate Square/182187129.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main()
+{
+    long long t;
+    cin>>t;
+    while(t--)
+    {
+        double n;
+        cin>>n;
+        double x=n/2;
+         cout<<setprecision(0)<<fixed<<ceil(x)<<endl;
+    }
+    return 0;
+}
