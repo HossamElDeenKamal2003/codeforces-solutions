1fbcd2538cec53fe7140c84192ae725632ef782a
Hossam El_Deen Kamal <101903513+HossamElDeenKamal2003@users.noreply.github.com>
2024-06-05 02:35:15 +0300

    Committing files

---
+++ CodeForces/C++17 (GCC 7-32)/479A | Expression/180527558.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    
+ long long ans, a, b, c;
+ cin >> a >> b >> c;
+ ans = a + b + c;
+ ans = max(ans, (a * b * c));
+ ans = max(ans, (a + b) * c);
+ ans = max(ans, a * (b + c));
+ ans = max(ans, a + (b * c));
+ ans = max(ans, (a * b) + c);
+ cout << ans << endl;
+    return 0;
+}
