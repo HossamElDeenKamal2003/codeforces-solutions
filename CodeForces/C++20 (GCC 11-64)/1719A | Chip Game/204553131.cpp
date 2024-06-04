de89c34fa8143dca6015a907fedb946b0949fd68
Hossam El_Deen Kamal <101903513+HossamElDeenKamal2003@users.noreply.github.com>
2024-06-05 02:25:01 +0300

    Committing files

---
+++ CodeForces/C++20 (GCC 11-64)/1719A | Chip Game/204553131.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+string solve(long long n, long long m){
+    long long sum = 0;
+    sum = n + m;
+    string tonya = "Tonya";
+    string burenka = "Burenka";
+    if(sum%2==0)
+        return tonya;
+    else
+        return burenka;
+ 
+ 
+}
+int main()
+{
+    int t;
+    cin >> t;
+    long long n, m;
+    while(t--){
+        cin >> n >> m;
+        cout << solve(n, m) << endl;
+    }
+}
+//9
+//1 2 3 4 2 5 1 6 7
