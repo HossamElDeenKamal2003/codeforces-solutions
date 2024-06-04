b06099ef2239c186aca1575eb668fe5ef1d8b9df
Hossam El_Deen Kamal <101903513+HossamElDeenKamal2003@users.noreply.github.com>
2024-06-05 02:26:59 +0300

    Committing files

---
+++ CodeForces/C++17 (GCC 7-32)/579A | Raising Bacteria/196541412.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define bs __builtin_popcount()
+using namespace std;
+ 
+int main()
+{
+    int x;
+    cin >> x;
+    cout << __builtin_popcount(x);
+ 
+}
