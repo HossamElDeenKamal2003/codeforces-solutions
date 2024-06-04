d04cc24ad6127f5445d75c692b5b76880ea3af82
Hossam El_Deen Kamal <101903513+HossamElDeenKamal2003@users.noreply.github.com>
2024-06-05 02:37:20 +0300

    Committing files

---
+++ CodeForces/C++17 (GCC 7-32)/231A | Team/162702455.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main()
+{
+    int n;
+    cin>>n;
+    int sum=0;
+    for (int i=1;i<=n;i++)
+    {
+        int sum2;
+        sum2==sum;
+        int p , v, t;
+        cin>>p>>v>>t;
+       // for (int j=1 ; j<=i;j++)
+ 
+           if (p+v+t>=2)
+            sum+=1;
+ 
+    }
+   cout <<sum;
+    return 0;
+}
