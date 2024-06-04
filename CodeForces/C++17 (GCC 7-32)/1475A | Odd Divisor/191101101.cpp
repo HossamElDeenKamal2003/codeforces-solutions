c5b2ea65ec0032ac0d502a92944a34d6be01c985
Hossam El_Deen Kamal <101903513+HossamElDeenKamal2003@users.noreply.github.com>
2024-06-05 02:30:19 +0300

    Committing files

---
+++ CodeForces/C++17 (GCC 7-32)/1475A | Odd Divisor/191101101.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <map>
+using namespace std;
+/*int solve() {
+ int n, ans = -1;
+ cin >> n;
+ map<int, int>mp;
+ for (int i = 1; i <= n; i++) {
+  int x;
+  cin >> x;
+  mp[x]++;
+  if (mp[x] >= 3)
+   ans = x;
+ }
+ return ans;
+}*/
+void HossamFast() {
+ ios_base::sync_with_stdio(false);
+ cin.tie(NULL);
+ cout.tie(NULL);
+}
+void solve() {
+ long long n;
+ cin >> n;
+ if (n & 1) {
+  cout << "YES" << endl;
+ }
+ else {
+  bool flag = false;
+  long long  div = n;
+  while (div > 2) {
+   div /= 2;
+   if (div & 1) {
+    if (n % div == 0) {
+     flag = true;
+     break;
+    }
+   }
+  }
+  if (flag) {
+   cout << "YES" << endl;
+  }
+  else {
+   cout << "NO" << endl;
+  }
+ }
+}
+int main()
+{
+ /*int t;
+ cin >> t;
+ while (t--) {
+  cout << solve();
+  cout << endl;
+ }*/
+ /*int n;
+ cin >> n;
+ vector<int>vec;
+ for (int i = 0; i < n; i++) {
+  int x;
+  cin >> x;
+  vec.push_back(x);
+  if (vec[i] == 1) {
+   cout << -1<<endl;
+   exit(0);
+  }
+ }
+ cout << 1<<endl;*/
+ /*int h, m, t, day, time;
+ cin >> t;
+ while (t--)
+ {
+  time = 0;
+  cin >> h >> m;
+  day = 24 * 60;
+  time = h * 60 + m;
+  cout << day - time << endl;
+ }*/
+ HossamFast();
+ int t;
+ cin >> t;
+ while (t--) {
+  solve();
+ }
+}
