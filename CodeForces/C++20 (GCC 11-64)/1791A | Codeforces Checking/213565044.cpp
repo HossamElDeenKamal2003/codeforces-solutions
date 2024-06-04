ee56540dd48eca8b568b2f570474ee9b4c979d58
Hossam El_Deen Kamal <101903513+HossamElDeenKamal2003@users.noreply.github.com>
2024-06-05 02:23:43 +0300

    Committing files

---
+++ CodeForces/C++20 (GCC 11-64)/1791A | Codeforces Checking/213565044.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+// fast cout and cin function
+void HossamFast(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+}
+void solve(){
+    char ch;
+    cin >> ch;
+    if(ch == 'c')
+        cout << "YES" << endl;
+    else if(ch == 'o')
+        cout << "YES" << endl;
+    else if(ch == 'd')
+        cout << "YES" << endl;
+    else if(ch == 'e')
+        cout << "YES" << endl;
+    else if(ch == 'f')
+        cout << "YES" << endl;
+    else if(ch == 'o')
+        cout << "YES" << endl;
+    else if(ch == 'r')
+        cout << "YES" << endl;
+    else if(ch == 'c')
+        cout << "YES" << endl;
+    else if(ch == 'e')
+        cout << "YES" << endl;
+    else if(ch == 's')
+        cout << "YES" << endl;
+    else
+        cout << "NO" << endl;
+}
+int main()
+{
+    HossamFast();
+    // C O D E F O R C E S
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+}
